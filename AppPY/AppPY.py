93c67da664a9f5ef97091c924f0d9ec0a323b9af
pedrojorge188 <pedrorodriguesjorge03@gmail.com>
2022-09-01 16:55:32 +0100

    push app

--- AppPY/AppPY.py
+++ AppPY/AppPY.py
@@ -5,8 +5,28 @@ import random
 import string
 
 Run = False
+Passwords_List = []
 
 app.theme("DarkAmber")
+
+def restoreDb():
+    with open('passwords.txt','r') as restore:
+
+        line = restore.readline()
+
+        if(line != ''):
+            Passwords_List.append(line)
+
+        while line != '':
+            line = restore.readline()
+            Passwords_List.append(line)
+
+        
+
+def savePassword(password):
+      with open("passwords.txt",'a') as file:
+                file.write(password+'\n')
+      app.popup("Password Salva:")
 
 def generatePassword(size):
     create = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation)
@@ -19,13 +39,25 @@ def initLayout(generatedPassword):
     layout = [[app.Text('Python Password Generator')],
               [app.Combo(['Pequena','Media', 'Grande'],readonly = True,text_color = 'white',default_value='Escolha o Tamanho...',key='selection',size=(20,1))],
               [app.Input(default_text=generatedPassword,readonly = True,text_color = 'black')],
-              [app.Button("Generate")]]
+              [app.Button("Generate")],
+              [app.Button("Save Password")],
+              [app.Button("Password Database")]]
 
     window = app.Window("Password Generation", layout)
-
     return window
 
+def DBLayout():
+    layout = [[app.Text('DataBase -> Saved Passwords')],[app.Text(data)                                     
+                for data in range(Passwords_List)]
+             ]
+
+    window = app.Window("DataBase", layout)
+    return window
+
+
 while True:
+
+    restoreDb()
 
     if Run == True:
         window.close()
@@ -42,7 +74,21 @@ while True:
         elif values['selection'] == 'Grande':
             password = generatePassword(50)
         else:
-            app.popup("Introuza o tamanho da password")
+            app.popup(PASSERROR)
+    elif event == "Save Password":
+        if password == DEFAULT:
+            app.popup(PASSERROR)
+        else:
+            savePassword(password)
+    elif event == 'Password Database':
+        if Passwords_List[0] != '':
+            restoreDb()
+
+            dataBaseWindow = DBLayout()
+            e,v = dataBaseWindow.read()
+
+        else:
+            app.popup(PASSERROR)
     elif event == app.WIN_CLOSED:
         break
        
--- AppPY/constants.py
+++ AppPY/constants.py
@@ -1,2 +1,5 @@
 MAX_SIZE = 20
 password = 'Please Enter Button To Generate a Password!'
+DEFAULT = 'Please Enter Button To Generate a Password!'
+counter = 0
+PASSERROR = "Nenhuma password Disponivel"
